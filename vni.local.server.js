c7aceb9e20669cc02daebf530df3516b7777118d
Tarlja <lenatarlja@gmail.com>
2026-06-02 13:25:46 +0300

    Change server binding from localhost to all interfaces

--- vni.local.server.js
+++ vni.local.server.js
@@ -103,6 +103,6 @@ const server = http.createServer(async (req, res) => {
 });
 
 const PORT = 8080
-server.listen(PORT, '127.0.0.1', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
